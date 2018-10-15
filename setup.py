7ab5339433f16ba7cd106545683b9f8ab19e94fa
jgj-magenta-aps <jgj@magenta-aps.dk>
2018-10-15 15:55:52 +0200

    bumbed version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 
 setup(
     name='service_person_stamdata_udvidet',
-    version='0.1.1',
+    version='0.1.2',
     description='',
     author='Magenta ApS',
     author_email='info@magenta.dk',
